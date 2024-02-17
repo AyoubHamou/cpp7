1ae78c6c9f4e1bf8a98b9af4231eec9aba16de75
Ayoubhamou <ahamou@student.1337.ma>
2024-02-17 23:12:36 +0100

    Fixed the copy constructor problem

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -68,15 +68,23 @@ int main() {
   try {
     Array<int> ar(5);
     Array<std::string> str(2);
-    {
-      Array<int> cp;
-      cp = ar;
-      std::cout << "Cp is " << cp[4] << std::endl;
-    }
+
     ar[0] = 10;
     ar[1] = 20;
     str[0] = "Ayoub";
     str[1] = "Hamou";
+
+    {
+      Array<int> cp; // test = operator
+      cp = ar;
+      cp[0] = 1;
+      std::cout << "Cp[0] is " << cp[0] << std::endl;
+      std::cout << "Cp[1] is " << cp[1] << std::endl;
+    }
+    {
+      Array<std::string> str2(str); // test copy construtor
+      std::cout << str2[0] << std::endl;
+    }
     std::cout << ar[1] << std::endl;
     std::cout << "(" << str[0] << ")" << std::endl;
   } catch (std::exception &e) {
